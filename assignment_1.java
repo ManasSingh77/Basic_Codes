dce5a43d78c025efed4fbb6e4d511bd40b2b9fb8
Manas Singh <122530839+ManasSingh77@users.noreply.github.com>
2023-09-12 13:47:44 +0530

    Add files via upload

---
+++ assignment_1.java
@@ -0,0 +1,20 @@
+package Assigment2;
+import java.util.Scanner;
+public class assignment_1 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        while(a<=b){
+        	int d=a-32;
+            int C=(5/9)*(d);
+            System.out.println(a+"\t"+C);
+            a+=c;
+        }
+			
+		
+
+	}
+
+}
